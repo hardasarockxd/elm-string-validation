465ae9e9d53b48596d3d01f462d39b436b25d266
hardasarockxd <hardasarockgg@gmail.com>
2017-12-27 17:31:40 +0200

    new function for input validation

--- StringValidation.elm
+++ StringValidation.elm
@@ -1,4 +1,4 @@
-module StringValidation exposing (..)
+module Helpers.StringValidation exposing (..)
 
 -- STRING VALIDATION
 -- "stringValidation" function
@@ -55,3 +55,97 @@ stringValidationToString ( string, bool ) =
         Tuple.first ( string, bool )
     else
         "VALIDATION ERROR"
+
+
+
+-- "nputValidation" function
+--  *FIRST ARGUMENT SHOULD BE NAME OF INPUT
+--  *SECOND ARGUMENT SHOULD BE STRING TO VALIDATE
+--  *THIRD ARGUMENT SHOULD BE  LIST OF SYMBOLS WHICH MUST BE IN STRING
+--  *FOURTH ARGUMENT SHOULD BE LIST OF SYMBOLS WHICH MUST NOT BE IN STRING
+--  *FIFTH ARGUMENT SHOULD BE MIN LENGTH OF STRING
+--  *SIXTH ARGUMENT SHOULD BE MAX LENGTH OF STRING
+
+
+inputValidation : String -> String -> List Char -> List Char -> Int -> Int -> ( String, Bool, String )
+inputValidation inputName string listOfSymbols listOfBanSymbols startPoint endPoint =
+    let
+        stringLength =
+            String.length string
+
+        charValidation =
+            List.map
+                (\char ->
+                    let
+                        newChar =
+                            toString char
+                                |> String.dropLeft 1
+                                |> String.dropRight 1
+                    in
+                    String.contains newChar string
+                )
+                listOfSymbols
+
+        listBoolValidation =
+            List.filter
+                (\bool -> bool)
+                charValidation
+
+        charBanValidation =
+            List.map
+                (\char ->
+                    let
+                        newChar =
+                            toString char
+                                |> String.dropLeft 1
+                                |> String.dropRight 1
+                    in
+                    String.contains newChar string
+                )
+                listOfBanSymbols
+
+        listBoolBanValidation =
+            List.filter
+                (\bool -> not bool)
+                charBanValidation
+    in
+    if stringLength <= startPoint then
+        ( string, False, "Text should be no less than " ++ toString startPoint ++ " symbols " )
+    else if stringLength >= endPoint then
+        ( string, False, "Text should be no bigger than " ++ toString endPoint ++ " symbols " )
+    else if List.length charValidation /= List.length listBoolValidation then
+        let
+            listOfAllSymbols =
+                List.map
+                    (\char ->
+                        toString char
+                            |> String.dropLeft 1
+                            |> String.dropRight 1
+                    )
+                    listOfSymbols
+
+            stringWithSymbols =
+                String.join ", " listOfAllSymbols
+        in
+        ( string, False, "Text should include " ++ stringWithSymbols )
+    else if List.length charBanValidation /= List.length listBoolBanValidation then
+        if String.length inputName == 0 then
+            let
+                listOfAllBanSymbols =
+                    List.map
+                        (\char ->
+                            toString char
+                                |> String.dropLeft 1
+                                |> String.dropRight 1
+                        )
+                        listOfBanSymbols
+
+                stringWithBanSymbols =
+                    String.join ", " listOfAllBanSymbols
+            in
+            ( string, False, "Text should not contain:" ++ stringWithBanSymbols )
+        else
+            ( string, False, "Please, type your real " ++ inputName )
+    else
+        ( string, True, "" )
+
